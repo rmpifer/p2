ef0fb10ebe97f8d3577bd01bd6ae8b157e8d1031
Ryan Pifer <rmpifer@umich.edu>
2016-10-14 00:01:17 +0000

    fp2

--- controllers/main.py
+++ controllers/main.py
@@ -9,7 +9,7 @@ def test_route():
 	db = connect_to_database()
 	cur = db.cursor()
 
-
+	results = []
 	firstname = ""
 	lastname = ""
 	if 'username' in session:
@@ -23,15 +23,24 @@ def test_route():
 		cur.execute('SELECT albumID FROM Album WHERE access=%s OR username=%s UNION \
 		SELECT albumID FROM AlbumAccess WHERE username=%s', ('public', session['username'], session['username']))
 		results = cur.fetchall()
+
 	else:
 		inSession = False
 		cur.execute('SELECT albumID FROM Album WHERE access=%s', ['public'])
 		results = cur.fetchall()
 
+	albums = []
+	for result in results:
+		cur.execute('SELECT title, albumID FROM Album WHERE albumID=%s', [result['albumID']])
+		albumTitle = cur.fetchall()
+		albumTitle = albumTitle[0]['title']
+		albums.append(albumTitle)
+
 	cur.execute('SELECT DISTINCT username FROM Album WHERE access=%s', ['public'])
 	users = cur.fetchall()
+
 	options = { 
-		"results": results,
+		"albums": albums,
 		"inSession": inSession,
 		"users": users,
 		"firstname": firstname,
